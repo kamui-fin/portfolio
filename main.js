62057ab3d0dafb5e102a76f3548f50fa36761a08
Kamui <fin-kamui@pm.me>
2021-06-08 14:21:38 -0500

    video timestamp functionality

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function toggleNav() {
 
 const hamburgerButton = document.querySelector("button.hamburger");
 hamburgerButton.addEventListener("click", toggleNav);
+
+// Video demo functionality
+const firstProject = document.querySelector(".project");
+const video = firstProject.querySelector("video");
+const timestamps = firstProject.querySelectorAll(".time");
+timestamps.forEach((e) => {
+    e.addEventListener("click", (t) => {
+        const [m, s] = t.target.text
+            .split(":")
+            .slice(0, 2)
+            .map((e) => Number.parseInt(e));
+
+        const sec = m * 60 + s;
+        video.currentTime = sec;
+    });
+});
