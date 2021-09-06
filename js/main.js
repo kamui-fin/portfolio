7946e60a717a7bed6c413a1134dfaea1a007daca
Kamui <fin-kamui@pm.me>
2021-09-06 07:28:25 -0500

    fix: tablet responsiveness

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 function toggleNav() {
-    const navBar = document.querySelector("nav.navbar");
-    navBar.classList.toggle("hidden");
+    const navBar = document.querySelector('nav.navbar')
+    navBar.classList.toggle('hidden')
 }
 
-const hamburgerButton = document.querySelector("button.hamburger");
-hamburgerButton.addEventListener("click", toggleNav);
+const hamburgerButton = document.querySelector('.menu-icon')
+hamburgerButton.addEventListener('click', toggleNav)
 
 // Video demo functionality
-const firstProject = document.querySelector(".project");
-const video = firstProject.querySelector("video");
-const timestamps = firstProject.querySelectorAll(".time");
+const firstProject = document.querySelector('.project')
+const video = firstProject.querySelector('video')
+const timestamps = firstProject.querySelectorAll('.time')
 timestamps.forEach((e) => {
-    e.addEventListener("click", (t) => {
+    e.addEventListener('click', (t) => {
         const [m, s] = t.target.text
-            .split(":")
+            .split(':')
             .slice(0, 2)
-            .map((e) => Number.parseInt(e));
+            .map((e) => Number.parseInt(e))
 
-        const sec = m * 60 + s;
-        video.currentTime = sec;
-    });
-});
+        const sec = m * 60 + s
+        video.currentTime = sec
+    })
+})
